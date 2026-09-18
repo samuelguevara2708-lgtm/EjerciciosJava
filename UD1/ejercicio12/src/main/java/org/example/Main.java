652b948bb37fdab75ccb26086934fff56c016095
guealasam <samuelguevara2708@gmail.com>
2026-09-18 18:04:35 +0200

    ejercicios 11 , 12 , 14

---
+++ UD1/ejercicio12/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+        int miInt = 9;
+        double midouble = 9;
+
+        System.out.println(miInt);
+        System.out.println(midouble);
+
+    }
+}
---
+++ UD1/ejercicio14/src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+        final int SEGUNDOS = 186_435;
+
+        System.out.println( SEGUNDOS + " segundos");
+
+        int minutos = SEGUNDOS / 60;
+        int segSr = SEGUNDOS % 60;
+
+        System.out.println( minutos + " minutos " + segSr+ " segundos");
+
+        int horas = minutos / 60;
+        int minutosSr = minutos % 60;
+
+        System.out.println(horas + " horas");
+
+        int dias = horas / 24;
+
+        int horasSr = horas % 24;
+
+        System.out.println(dias + " dias " + horasSr + " horas " + minutosSr + " minutos " + segSr + " segundos " );
+
+    }
+}
